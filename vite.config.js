2b176dd3ac1ecb17b8ea6642125e36a7ec58a6ba
Patcharapon Thepwijit <omsinpt@MacBook-Air-khxng-Patcharapon.local>
2025-09-03 02:35:07 +0700

    allow sinpw.site in allowededHosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
+    allowedHosts: ['sinpw.site','localhost'],
     host: true,
     port: 5173,
     watch: {
